d1dbf44f9f16c80d1086bd5967b0e1698174a6e8
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-04 15:40:48 +0100

    Worked on back button in city screen

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -8,6 +8,31 @@ class CityScreen extends StatefulWidget {
 class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/city_background.jpg"),
+                fit: BoxFit.cover)),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: FlatButton(
+                    onPressed: () {
+                      print("Clicked on back button");
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      size: 40,
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

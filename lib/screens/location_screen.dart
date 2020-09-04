31e73a25d902f6a448bf0f7d0a1b364ab9bfc539
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-04 14:39:57 +0100

    Working on location screen: refresh and city buttons

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -13,7 +13,34 @@ class _LocationScreenState extends State<LocationScreen> {
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/location_background.jpg"),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.9), BlendMode.dstATop)
           )
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  FlatButton(onPressed: (){
+                    print("Clickd");
+                  }, child: Icon(
+                    Icons.near_me,
+                    size: 40.0,
+                  )),
+                  FlatButton(onPressed: (){
+                    print("Clickd");
+                  }, child: Icon(
+                    Icons.near_me,
+                    size: 40.0,
+                  ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
